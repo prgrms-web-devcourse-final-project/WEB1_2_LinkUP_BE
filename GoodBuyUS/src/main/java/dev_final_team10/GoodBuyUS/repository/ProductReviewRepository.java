7ea6dbcf6158666843078d8ce09fcf31d132ef71
Gongjjin <gjyj5395@naver.com>
2024-11-22 03:20:22 +0900

    feat : 상품리뷰레포지토리 생성

---
+++ GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/repository/ProductReviewRepository.java
@@ -0,0 +1,7 @@
+package dev_final_team10.GoodBuyUS.repository;
+
+import dev_final_team10.GoodBuyUS.domain.ProductReview;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductReviewRepository extends JpaRepository<ProductReview, Long> {
+}
