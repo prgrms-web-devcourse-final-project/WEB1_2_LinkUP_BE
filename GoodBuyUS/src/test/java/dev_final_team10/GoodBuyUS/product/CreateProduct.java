129baa386a2f3e70ebc14c847f4963332d48f392
Gongjjin <gjyj5395@naver.com>
2024-11-22 02:35:26 +0900

    refactor : @Transactional 제거

--- GoodBuyUS/src/test/java/dev_final_team10/GoodBuyUS/product/CreateProduct.java
+++ GoodBuyUS/src/test/java/dev_final_team10/GoodBuyUS/product/CreateProduct.java
@@ -9,19 +9,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.transaction.annotation.Transactional;
 
-@Transactional
 @SpringBootTest
 @ActiveProfiles("test")
 @DisplayName("상품의 카테고리가 올바르게 설정되는지 테스트")
 public class CreateProduct {
 
     @Test
-    @DisplayName("카테고리 - 티셔츠 테스트")
+    @DisplayName("상단 상품 - 리뷰 리스트 연결 확인")
     public void create(){
-        Product product =   new Product(1L,"진짜 멋진 옷", 100000, "사진 이미지", ProductCategory.FASHION, SubCategory.CHILDREN_CLOTHING, DetailCategory.KIDS_CLOTHING_PANTS);
-        Assertions.assertThat(product.getProductCategory()).isEqualTo(ProductCategory.FASHION);
+        Product product = Product.createProduct("유아용 바지",10000, "qqwe",ProductCategory.FASHION, SubCategory.CHILDREN_CLOTHING, DetailCategory.KIDS_CLOTHING_PANTS);
+        Assertions.assertThat(product.getReviews().size()).isEqualTo(0);
     }
-
 }
