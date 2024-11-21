bc28f39c40a412b31e517544b27c5e4d63e26280
Gongjjin <gjyj5395@naver.com>
2024-11-22 02:41:32 +0900

    test : 리뷰 생성 테스트

---
+++ GoodBuyUS/src/test/java/dev_final_team10/GoodBuyUS/review/CreateReview.java
@@ -0,0 +1,34 @@
+package dev_final_team10.GoodBuyUS.review;
+
+import dev_final_team10.GoodBuyUS.domain.Product;
+import dev_final_team10.GoodBuyUS.domain.ProductReview;
+import dev_final_team10.GoodBuyUS.domain.category.DetailCategory;
+import dev_final_team10.GoodBuyUS.domain.category.ProductCategory;
+import dev_final_team10.GoodBuyUS.domain.category.SubCategory;
+import dev_final_team10.GoodBuyUS.repository.ProductRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@SpringBootTest
+@ActiveProfiles("test")
+public class CreateReview {
+    @Autowired
+    ProductRepository productRepository ;
+    @Test
+    @DisplayName("리뷰 생성 테스트")
+    public void Review(){
+        Product product = Product.createProduct("유아용 바지",10000, "qqwe",ProductCategory.FASHION, SubCategory.CHILDREN_CLOTHING, DetailCategory.KIDS_CLOTHING_PANTS);
+        productRepository.save(product);
+        /**
+         * productReview에 product는 프록시 상태 ?
+         */
+        ProductReview productReview = ProductReview.createProductReview(product,"이거 예뻐요",5);
+        Assertions.assertThat(productReview.getProduct().getProductId()).isEqualTo(1L);
+    }
+}
