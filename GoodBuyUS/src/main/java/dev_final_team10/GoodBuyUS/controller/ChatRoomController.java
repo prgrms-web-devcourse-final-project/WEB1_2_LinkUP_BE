8f599bdb26086775215e90ca3ceaec9b3c55ec2d
sungjun2018 <tjdwns3897@gmail.com>
2024-11-28 12:17:05 +0900

    refactor: 채팅방 엔드포인트 수정

--- GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/controller/ChatRoomController.java
+++ GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/controller/ChatRoomController.java
@@ -1,7 +1,5 @@
 package dev_final_team10.GoodBuyUS.controller;
 
-import com.auth0.jwt.JWT;
-import com.sun.security.auth.UserPrincipal;
 import dev_final_team10.GoodBuyUS.domain.chat.ChatRoom;
 import dev_final_team10.GoodBuyUS.domain.chat.ChatRoomDTO;
 import dev_final_team10.GoodBuyUS.jwt.JwtService;
@@ -10,35 +8,31 @@ import dev_final_team10.GoodBuyUS.service.ChatRoomService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/chat")
 public class ChatRoomController {
     private final ChatRoomService chatRoomService;
     private final JwtService jwtService;
     private final UserRepository userRepository;
 
     //채팅방 생성
-    @PostMapping()
+    @PostMapping("/chat")
     public ResponseEntity<ChatRoom> createChatRoom(@RequestBody ChatRoomDTO chatRoomDTO) {
         return ResponseEntity.ok(chatRoomService.createChatRoom(chatRoomDTO));
     }
 
     //채팅방 삭제
-    @DeleteMapping("/{chatRoomId}")
+    @DeleteMapping("/chat/{chatRoomId}")
     public void removeChatRoom(@PathVariable Long chatRoomId) {
         chatRoomService.removeChatRoom(chatRoomId);
     }
 
     //채팅방 목록
-    @GetMapping("/list")
+    @GetMapping("/mypage/chatlist")
     public List<ChatRoomDTO> chatRoomList(HttpServletRequest request) {
         String accessToken = jwtService.extractAccessToken(request).orElseThrow(()->new RuntimeException("액세스 토큰이 잘못되었습니다"));
         String email = jwtService.extractEmail(accessToken).orElseThrow(()->new RuntimeException("액세스 토큰이 잘못되었습니다"));
