669b47a1c8dda16ac78633bd956a2519c4d27570
LChaeWon <dcodnjs12@naver.com>
2024-11-28 12:12:32 +0900

    feat : 동네 변경 기능 추가

--- GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/service/MypageService.java
+++ GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/service/MypageService.java
@@ -1,14 +1,19 @@
 package dev_final_team10.GoodBuyUS.service;
 
+import dev_final_team10.GoodBuyUS.domain.neighborhood.Neighborhood;
 import dev_final_team10.GoodBuyUS.domain.user.User;
+import dev_final_team10.GoodBuyUS.repository.NeighborhoodRepository;
 import dev_final_team10.GoodBuyUS.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Map;
 
 @Service
 @Transactional
@@ -17,6 +22,8 @@ public class MypageService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final UserService userService;
+    private final NeighborhoodRepository neighborhoodRepository;
 
     //현재 로그인한 사용자의 이메일을 가져오는 메소드
     public String getCurrentUserEmail() {
@@ -42,4 +49,20 @@ public class MypageService {
         userRepository.save(user);
     }
 
+    //동네 변경하는 메소드
+    public ResponseEntity<?> chageNeighbor(String newAddress) {
+        //새로 입력한 주소로 동네테이블에서 해당하는 행 찾기
+        String neighborhoodName =  userService.extractNeighborhoodName(newAddress);
+        Neighborhood neighborhood = neighborhoodRepository.findByNeighborhoodName(neighborhoodName);
+
+        //현재 로그인한 사용자 정보 가져오기
+        String email = getCurrentUserEmail();
+        User user = userRepository.findByEmail(email).orElse(null);
+
+        user.setAddress(newAddress);
+        user.setNeighborhood(neighborhood);
+        userRepository.save(user);
+
+        return ResponseEntity.ok(Map.of("message","주소가 성공적으로 변경되었습니다."));
+    }
 }
