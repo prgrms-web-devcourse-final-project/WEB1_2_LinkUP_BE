75ec7c0df3423e60f72a7325dd47618b186a7a23
sungjun2018 <tjdwns3897@gmail.com>
2024-11-28 11:47:21 +0900

    refactor: userId 대신에 토큰 정보로 조회

--- GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/controller/ChatRoomController.java
+++ GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/controller/ChatRoomController.java
@@ -1,10 +1,18 @@
 package dev_final_team10.GoodBuyUS.controller;
 
+import com.auth0.jwt.JWT;
+import com.sun.security.auth.UserPrincipal;
 import dev_final_team10.GoodBuyUS.domain.chat.ChatRoom;
 import dev_final_team10.GoodBuyUS.domain.chat.ChatRoomDTO;
+import dev_final_team10.GoodBuyUS.jwt.JwtService;
+import dev_final_team10.GoodBuyUS.repository.UserRepository;
 import dev_final_team10.GoodBuyUS.service.ChatRoomService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,6 +22,8 @@ import java.util.List;
 @RequestMapping("/chat")
 public class ChatRoomController {
     private final ChatRoomService chatRoomService;
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
 
     //채팅방 생성
     @PostMapping()
@@ -28,8 +38,12 @@ public class ChatRoomController {
     }
 
     //채팅방 목록
-    @GetMapping("/{userId}")
-    public List<ChatRoomDTO> chatRoomList(@PathVariable Long userId) {
+    @GetMapping("/list")
+    public List<ChatRoomDTO> chatRoomList(HttpServletRequest request) {
+        String accessToken = jwtService.extractAccessToken(request).orElseThrow(()->new RuntimeException("액세스 토큰이 잘못되었습니다"));
+        String email = jwtService.extractEmail(accessToken).orElseThrow(()->new RuntimeException("액세스 토큰이 잘못되었습니다"));
+        Long userId = userRepository.findByEmail(email).orElseThrow(()-> new RuntimeException("사용자를 찾을 수 없습니다")).getId();
+
         return chatRoomService.getListChatRoom(userId);
     }
 }
