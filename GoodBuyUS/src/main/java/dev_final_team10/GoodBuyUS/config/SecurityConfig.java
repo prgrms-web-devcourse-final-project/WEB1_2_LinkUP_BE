60c22011f484172630f0e037a091b11262fc44ba
Gongjjin <gjyj5395@naver.com>
2024-11-30 00:12:31 +0900

    refactor : 임시로 URL 권한 변경

--- GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/config/SecurityConfig.java
+++ GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/config/SecurityConfig.java
@@ -51,7 +51,6 @@ public class SecurityConfig {
                 .requestMatchers("/","/css/**","/images/**","/js/**","/favicon.ico","/h2-console/**","/homepage","/users/find","/users/reset").permitAll()
                 .requestMatchers(HttpMethod.POST, "/goodbuyUs/order/**").authenticated() // POST 요청 제한
                 .requestMatchers(HttpMethod.GET, "/goodbuyUs/order/**").authenticated() // POST 요청 제한
-                .requestMatchers("/","/css/**","/images/**","/js/**","/favicon.ico","/h2-console/**","/goodbuyUs").permitAll()
                 .requestMatchers("/users").permitAll() // 회원가입 접근 가능
                 .anyRequest().authenticated() // 위의 경로 이외에는 모두 인증된 사용자만 접근 가능
                 );
