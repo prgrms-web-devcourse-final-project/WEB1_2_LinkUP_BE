cd3763c7237c302a188d4c816d2a9ad3f4a2a6d1
Gongjjin <gjyj5395@naver.com>
2024-11-29 23:35:55 +0900

    홍공진 작업 부분 충돌 해결2

---
+++ GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/domain/payment/PaymentDTO.java
@@ -0,0 +1,29 @@
+package dev_final_team10.GoodBuyUS.domain.payment;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data @NoArgsConstructor @AllArgsConstructor
+public class PaymentDTO {
+    private PaymentStatus status;
+    private String paymentKey;
+    private UUID orderId;
+    private int amount;
+    private String message;
+    private String redirectURL;
+
+    public PaymentDTO(PaymentStatus status, String paymentKey, UUID orderId, int amount) {
+        this.status = status;
+        this.paymentKey = paymentKey;
+        this.orderId = orderId;
+        this.amount = amount;
+    }
+
+    public PaymentDTO(PaymentStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
