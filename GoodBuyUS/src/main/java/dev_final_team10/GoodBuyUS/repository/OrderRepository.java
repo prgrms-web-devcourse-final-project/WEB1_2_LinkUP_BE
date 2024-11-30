5531fa36055fb3836a75a59b0eeef0204645ca4b
Gongjjin <gjyj5395@naver.com>
2024-11-30 21:28:04 +0900

    feat : 주문된 상품의 글(어쩌다보니 중간 테이블)을 조건을 달아서 조회하는 JPQL 추가

--- GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/repository/OrderRepository.java
+++ GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/repository/OrderRepository.java
@@ -1,10 +1,13 @@
 package dev_final_team10.GoodBuyUS.repository;
 
 import dev_final_team10.GoodBuyUS.domain.order.entity.Order;
+import dev_final_team10.GoodBuyUS.domain.order.entity.OrderStatus;
 import dev_final_team10.GoodBuyUS.domain.product.entity.Product;
 import dev_final_team10.GoodBuyUS.domain.product.entity.ProductPost;
 import dev_final_team10.GoodBuyUS.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,4 +17,7 @@ import java.util.UUID;
 public interface OrderRepository extends JpaRepository<Order, UUID> {
     public List<Order> findOrderByUser(User user);
     public List<Order> findAllByProductPost(ProductPost product);
+
+    @Query("SELECT o FROM Order o WHERE o.productPost = :productPost AND o.orderStatus = :status")
+    List<Order> findAllByProductPostAndStatus(@Param("productPost") ProductPost productPost, @Param("status") OrderStatus status);
 }
