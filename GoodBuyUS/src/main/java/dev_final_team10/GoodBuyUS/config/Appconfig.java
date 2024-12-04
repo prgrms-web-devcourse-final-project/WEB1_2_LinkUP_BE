ef089a424a33dccd3e71da51c87a39acef74f39e
Gongjjin <gjyj5395@naver.com>
2024-12-04 12:19:15 +0900

    refactor : 프론트와 연동을 위해 프론트 주소 추가

--- GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/config/Appconfig.java
+++ GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/config/Appconfig.java
@@ -9,7 +9,7 @@ public class Appconfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
-                .allowedOrigins("http://15.164.5.135") // 허용할 출처
+                .allowedOrigins("http://localhost:5173") // 허용할 출처
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 인증 정보 허용
