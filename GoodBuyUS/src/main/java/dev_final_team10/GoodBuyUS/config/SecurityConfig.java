a7fd91025853f81f59db374ad3b20c5b23de14e1
josh971230 <brionlee97@naver.com>
2024-12-04 11:45:08 +0900

    fix: 병합후 충돌 해결

--- GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/config/SecurityConfig.java
+++ GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/config/SecurityConfig.java
@@ -50,8 +50,7 @@ public class SecurityConfig {
                         .requestMatchers("/naver/**").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")// 네이버 OAuth 경로 허용
                         .requestMatchers(HttpMethod.POST, "/goodbuyUs/order/**").authenticated() // 주문 관련 요청 제한
-                        .requestMatchers(HttpMethod.GET, "/goodbuyUs/order/**").authenticated()
-                        .anyRequest().authenticated() // 나머지 요청은 인증 필요
+                        .requestMatchers(HttpMethod.GET, "/goodbuyUs/order/**").authenticated()// 나머지 요청은 인증 필요
                 // 아이콘, css, js 관련
                 // 기본 페이지, css, image, js 하위 폴더에 있는 자료들은 모두 접근 가능, h2-console에 접근 가능
                 .requestMatchers(HttpMethod.POST, "/goodbuyUs/order/**").authenticated() // POST 요청 제한
