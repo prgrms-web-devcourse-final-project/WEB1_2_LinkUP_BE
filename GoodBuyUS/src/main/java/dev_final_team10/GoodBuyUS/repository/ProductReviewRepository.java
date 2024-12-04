99b793204cf1ec65c0e9df4afb322a069b661bcb
Gongjjin <gjyj5395@naver.com>
2024-12-05 03:16:30 +0900

    feat : 사용자가 쓴 리뷰 찾기

--- GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/repository/ProductReviewRepository.java
+++ GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/repository/ProductReviewRepository.java
@@ -1,9 +1,13 @@
 package dev_final_team10.GoodBuyUS.repository;
 
 import dev_final_team10.GoodBuyUS.domain.product.entity.ProductReview;
+import dev_final_team10.GoodBuyUS.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductReviewRepository extends JpaRepository<ProductReview, Long> {
+   public Optional<ProductReview> findByUserAndProductReviewId(User user, Long reviewId);
 }
