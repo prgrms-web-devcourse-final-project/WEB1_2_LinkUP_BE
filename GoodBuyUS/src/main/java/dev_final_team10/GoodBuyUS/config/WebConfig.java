6e54af1bbbb19b63fa460e54c39c09a42b6dca62
Gongjjin <gjyj5395@naver.com>
2024-12-04 17:01:55 +0900

    hotfix : cors 설정 추가

--- GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/config/WebConfig.java
+++ GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/config/WebConfig.java
@@ -7,8 +7,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
     public class WebConfig implements WebMvcConfigurer {
         @Override
         public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:5173")
+            registry.addMapping("/api/**")
+                    .allowedOrigins("http://localhost:5173","http://15.164.5.135")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
                     .allowCredentials(true);
