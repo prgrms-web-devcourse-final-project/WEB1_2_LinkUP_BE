094d6e3f32f8459d0ce29497ff5a3c800ee8926e
sumin2eee <sumin2eee@gmail.com>
2024-12-04 13:32:19 +0900

    fix:develop merge 중 발생하는 오류 해결(Appconfig)

--- GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/config/Appconfig.java
+++ GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/config/Appconfig.java
@@ -1,6 +1,11 @@
+package dev_final_team10.GoodBuyUS.config;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @Configuration
 public class Appconfig implements WebMvcConfigurer {
