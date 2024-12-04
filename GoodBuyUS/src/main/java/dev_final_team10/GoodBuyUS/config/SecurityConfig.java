ca41f034dc68cbbd773c155052954bb759982c04
Gongjjin <gjyj5395@naver.com>
2024-12-04 17:40:03 +0900

    hotfix : 권한 설정을 위한 테스트 커밋

--- GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/config/SecurityConfig.java
+++ GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/config/SecurityConfig.java
@@ -47,18 +47,19 @@ public class SecurityConfig {
                 //== URL별 권한 관리 옵션 ==
                 .authorizeRequests(authz -> authz
                                 // 아이콘, css, js 관련
-                                .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/favicon.ico", "/h2-console/**", "/homepage", "/users/**", "/success").permitAll() // 공용 URL
-                        .requestMatchers("/naver/**").permitAll()// 네이버 OAuth 경로 허용
-                        .requestMatchers("/admin/**").hasRole("ADMIN")//admin설정
-                        .requestMatchers(HttpMethod.POST, "/goodbuyUs/order/**").authenticated() // 주문 관련 요청 제한
-                        .requestMatchers(HttpMethod.GET, "/goodbuyUs/order/**").authenticated()// 나머지 요청은 인증 필요
+                .requestMatchers("/api/**").permitAll() // 회원가입 접근 가능, 임시 커밋
+                .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/favicon.ico", "/h2-console/**", "/homepage", "/users/**", "/success").permitAll() // 공용 URL
+                .requestMatchers("/naver/**").permitAll()// 네이버 OAuth 경로 허용
+                .requestMatchers("/admin/**").hasRole("ADMIN")//admin설정
+                .requestMatchers(HttpMethod.POST, "/goodbuyUs/order/**").authenticated() // 주문 관련 요청 제한
+                .requestMatchers(HttpMethod.GET, "/goodbuyUs/order/**").authenticated()// 나머지 요청은 인증 필요
                 // 기본 페이지, css, image, js 하위 폴더에 있는 자료들은 모두 접근 가능, h2-console에 접근 가능
                 .requestMatchers(HttpMethod.POST, "/goodbuyUs/order/**").authenticated() // POST 요청 제한
                 .requestMatchers(HttpMethod.GET, "/goodbuyUs/order/**").authenticated() // POST 요청 제한
                 .requestMatchers(HttpMethod.POST, "/api/v1/main-payments/success").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/v1/main-payments/success").permitAll()
                 .requestMatchers(HttpMethod.POST, "/api/v1/main-payments/fail").permitAll()
-                .requestMatchers( HttpMethod.GET,"goodbuyUs/**").permitAll()
+                .requestMatchers( HttpMethod.GET,"/goodbuyUs/**").permitAll()
 
                 .requestMatchers("/users/**").permitAll() // 회원가입 접근 가능
                 .anyRequest().authenticated() // 위의 경로 이외에는 모두 인증된 사용자만 접근 가능
