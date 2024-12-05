915d6451ca1a603eec84793ceb2cf90342fc8e3b
LChaeWon <dcodnjs12@naver.com>
2024-12-05 18:30:11 +0900

    fix : 충돌 해결

--- GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/repository/ParticipationsRepository.java
+++ GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/repository/ParticipationsRepository.java
@@ -4,6 +4,7 @@ import dev_final_team10.GoodBuyUS.domain.community.entity.Participations;
 import dev_final_team10.GoodBuyUS.domain.community.entity.participationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import dev_final_team10.GoodBuyUS.domain.community.entity.CommunityPost;
 
 import java.util.List;
 
@@ -11,4 +12,5 @@ import java.util.List;
 public interface ParticipationsRepository extends JpaRepository<Participations, Long> {
     List<Participations> findAllByCommunityPost_CommunityPostIdAndStatus(Long communityPostId, participationStatus status);
     List<Participations> findAllByCommunityPost_CommunityPostId(Long communityPostId);
+    List<Participations> findByCommunityPost(CommunityPost post);
 }
