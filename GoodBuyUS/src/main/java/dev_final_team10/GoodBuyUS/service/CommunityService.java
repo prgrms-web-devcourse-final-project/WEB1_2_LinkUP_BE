06c67d6bce9c0383e23ae14ea0bb596b809f5003
LChaeWon <dcodnjs12@naver.com>
2024-12-06 15:31:10 +0900

    fix

--- GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/service/CommunityService.java
+++ GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/service/CommunityService.java
@@ -122,7 +122,7 @@ public class CommunityService {
         //마지막 참가자인 경우 글의 상태가 결제 대기로 바뀌게 + 결제 데드라인 생성
         if(getParticipantCount(communityPost.getCommunityPostId()).equals(communityPost.getAvailableNumber())){
             communityPost.setStatus(postStatus.PAYMENT_STANDBY);
-            List<Participations> participationsList = participationsRepository.findAllByCommunityPost_CommunityPostIdAndStatus(communityPost.getCommunityPostId()1, participationStatus.JOIN);
+            List<Participations> participationsList = participationsRepository.findAllByCommunityPost_CommunityPostIdAndStatus(communityPost.getCommunityPostId(), participationStatus.JOIN);
             for (Participations participations1 : participationsList) {
                 participations1.setStatus(participationStatus.PAYMENT_STANDBY);
                 participationsRepository.save(participations);
