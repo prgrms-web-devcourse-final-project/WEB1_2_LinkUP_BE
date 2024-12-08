249a2e3cd457578ca619c8dede142f129235dbc1
LChaeWon <dcodnjs12@naver.com>
2024-12-08 23:52:35 +0900

    hotfix: 로고 읽는 방식 수정

--- GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/service/EmailService.java
+++ GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/service/EmailService.java
@@ -5,6 +5,7 @@ import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -18,15 +19,24 @@ public class EmailService {
     private final JavaMailSender mailSender;
 
     //메일 보내는 메소드
-    public void sendEmail(String to, String subject, String htmlContent) throws MessagingException, IOException {
+    public void sendEmailWithInlineImage(String to, String subject, String htmlContent, String imagePath) throws MessagingException, IOException {
         MimeMessage mimeMessage = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
 
+
+        // 이메일 기본 설정
         helper.setTo(to);
         helper.setSubject(subject);
         helper.setText(htmlContent, true);
-        FileSystemResource logo = new FileSystemResource(new ClassPathResource("static/images/logo.png").getFile());
-        helper.addInline("logo",logo);
+
+
+        // 이미지 첨부
+        Resource resource = new ClassPathResource(imagePath);
+        if (!resource.exists()) {
+            throw new IOException("Image not found at path: " + imagePath);
+        }
+        helper.addInline("logo", resource);
+
         mailSender.send(mimeMessage);
     }
 }
