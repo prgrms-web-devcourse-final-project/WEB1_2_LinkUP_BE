dce26706e41e880fd90b5265833626ddbe877c45
LChaeWon <dcodnjs12@naver.com>
2024-12-09 21:03:39 +0900

    hotfix: 접근 권한 수정

--- GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/config/SecurityConfig.java
+++ GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/config/SecurityConfig.java
@@ -64,6 +64,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/products/payment-fail/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/v1/virtual/success/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/virtual/success/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/community/post/{community_post_id}/participants").permitAll()
                         .requestMatchers("api/**").authenticated()
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()  // OPTIONS 요청은 모두 허용
                         .anyRequest().permitAll()
