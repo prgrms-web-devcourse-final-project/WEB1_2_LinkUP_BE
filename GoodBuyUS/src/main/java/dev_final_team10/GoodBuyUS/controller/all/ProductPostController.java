6326c76409e1e51b53d9779d7432dd9e2077af1b
Gongjjin <gjyj5395@naver.com>
2025-01-08 17:04:19 +0900

    refactor : 보안정책으로 인해 api 엔드포인트 수정

--- GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/controller/all/ProductPostController.java
+++ GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/controller/all/ProductPostController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/goodbuyUs")
+@RequestMapping("goodbuyUs")
 @RequiredArgsConstructor
 public class ProductPostController {
     private final ProductPostService productPostService;
