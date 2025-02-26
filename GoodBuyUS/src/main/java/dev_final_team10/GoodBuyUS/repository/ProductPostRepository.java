417508e078a1a5fccb35d8213d72d7f086975ee7
Gongjjin <gjyj5395@naver.com>
2025-02-26 16:30:54 +0900

    hotfix : 상품 판매기간 지났을 때 연장시키기

--- GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/repository/ProductPostRepository.java
+++ GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/repository/ProductPostRepository.java
@@ -2,8 +2,10 @@ package dev_final_team10.GoodBuyUS.repository;
 
 import dev_final_team10.GoodBuyUS.domain.product.entity.ProductPost;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -21,4 +23,9 @@ public interface ProductPostRepository extends JpaRepository<ProductPost, Long> 
            "LEFT JOIN FETCH p.reviews r " +
            "WHERE pp.postId = :id")
    ProductPost findDetailByPostId(Long id);
+
+   @Modifying
+   @Transactional
+   @Query(value = "UPDATE product_post p SET p.product_period = DATE_ADD(p.product_period, INTERVAL 10 DAY), p.available = true", nativeQuery = true)
+   void updateDate();
 }
