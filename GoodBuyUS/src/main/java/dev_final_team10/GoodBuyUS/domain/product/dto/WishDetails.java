5f388ef0778d4e2dc9bed5894fef4e4201c11541
Gongjjin <gjyj5395@naver.com>
2025-02-26 16:30:15 +0900

    hotfix : wish에 아이디 추가

--- GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/domain/product/dto/WishDetails.java
+++ GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/domain/product/dto/WishDetails.java
@@ -1,6 +1,6 @@
 package dev_final_team10.GoodBuyUS.domain.product.dto;
 
-import lombok.AllArgsConstructor;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,12 +8,14 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 public class WishDetails {
+    private Long productPostId;
     private String productName;
     private int productPrice;
     private String productImage;
     private LocalDateTime addedAt;
 
-    public WishDetails(String productName, int productPrice, String productImage, LocalDateTime addedAt) {
+    public WishDetails(Long productPostId, String productName, int productPrice, String productImage, LocalDateTime addedAt) {
+        this.productPostId = productPostId;
         this.productName = productName;
         this.productPrice = productPrice;
         this.productImage = productImage;
