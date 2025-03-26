4591a3953c67fa83b8e1f066c85b9ca9cec198f6
sungjun <tjdwns3897@gmail.com>
2025-03-26 17:25:58 +0900

    hotfix: 시큐리티 cors 도메인 변경

--- GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/config/SecurityConfig.java
+++ GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/config/SecurityConfig.java
@@ -85,9 +85,9 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedOrigin("http://localhost:5173");  // 허용된 출처
-        configuration.addAllowedOrigin("http://15.164.5.135");
+        configuration.addAllowedOrigin("https://goodbuyus.store");
         configuration.addAllowedOrigin("ws://localhost:5173"); // 웹소켓 허용
-        configuration.addAllowedOrigin("ws://15.164.5.135");
+        configuration.addAllowedOrigin("wss://goodbuyus.store");
         configuration.addAllowedMethod("*");  // 모든 HTTP 메소드 허용
         configuration.addAllowedHeader("*");  // 모든 헤더 허용
         configuration.setAllowCredentials(true);  // 자격 증명 허용
