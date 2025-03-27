d8f243804265cd511d45c9485018145bd13045a1
sungjun2018 <tjdwns3897@gmail.com>
2025-03-27 21:02:23 +0900

    fix: 엔드포인트 경로 수정, 모든 경로에서 /api 제거

--- GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/controller/api/OrderController.java
+++ GoodBuyUS/src/main/java/dev_final_team10/GoodBuyUS/controller/api/OrderController.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/orders")
+@RequestMapping("/v1/orders")
 @RequiredArgsConstructor
 public class OrderController {
     private final OrderService orderService;
